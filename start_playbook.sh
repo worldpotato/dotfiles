6b3592ba8e56f3ee9f42664f80bfc1be09db298b
worldpotato <mail@felixstrobel.de>
2022-09-04 20:24:26 +0200

    [ansible] add the option to pass ansible parameter

--- start_playbook.sh
+++ start_playbook.sh
@@ -1,4 +1,11 @@
 #! /bin/sh
-SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )" 
-git -C $SCRIPTPATH pull
-ansible-playbook dotfile.playbook -i hosts.yaml
+
+usage="[-h] <ansible-parameter>
+typical usage: ./start_playbook.sh --tags rofi --skip-tags install"
+if [ $1 == "-h" ]; then
+  echo "$usage"
+else
+  SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )" 
+  git -C $SCRIPTPATH pull
+  ansible-playbook dotfile.playbook -i hosts.yaml "$@"
+fi
