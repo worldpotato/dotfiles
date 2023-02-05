e626ab88ee5bd4cbc733a666629622c50049f7de
Worldpotato <mail@felixstrobel.de>
2023-02-05 17:30:09 +0100

    [nvim] remove unncessary require statements

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -1,32 +1,4 @@
 -- colorscheme
 require("settings_config")
 require("lazy_config")
-
--- require("worldpotato")
-
--- require("lightline_config")
-
--- setup plugins
-
---require("lsp_config")
---require("rust-tools_config") -- should be after the lsp_config otherwise some commands are not working
-
--- require("gitsigns_config")
--- require("nvim_commit_config")
--- require("completion_config")
--- require("debug_config")
--- require("formatter_config")
--- require("telescope_config")
---require("dressing_config")
--- require("noice_config")
---require("test_config")
--- require("autopairs_config")
--- require("auto-save_config")
--- require("treesitter_config")
--- require("lualine_config")
--- require("symbols-outline_config")
--- require("nvim-tree_config")
--- require("notifications_config")
---require("statuscolumn_config")
-
 require("keymaps")
