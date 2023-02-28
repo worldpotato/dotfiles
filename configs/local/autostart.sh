3cd37ad6323f4b923780635c761888beeb602d5a
Worldpotato <mail@felixstrobel.de>
2023-02-28 21:48:59 +0100

    add local folder

---
+++ configs/local/autostart.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# $HOME/.config/polybar/launch.sh & 2&> /dev/null
+/usr/bin/nm-applet & 2&> /dev/null
+/usr/bin/dunst &
+
+# lock the screen after 5 min
+xset s 300
+
+/usr/bin/thunderbird & 2&> /dev/null
+/usr/bin/firefox & 2&> /dev/null
+
+# $HOME/.screenlayout/tripple_monitor.sh
+xrandr --dpi 94
+$HOME/.local/share/background/setbg & 2&> /dev/null # set desktop background &
+
+picom --backend glx -b
+
+# because it needs that for some reason...
+sleep 10
+bluetoothctl power on
+# bluetoothctl power on 2&> /dev/null
